5569824e3484f8fb25a87d4a403efa8dbaff1b69
Jordanne Dale <dalejordanne@gmail.com>
2019-10-15 10:33:37 -0500

    Exercise 3 and 6 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,56 +4,89 @@ window.onload = function() {
       var i;
       for (i = 0; i < c.length; i++) {
           c[i].classList.add("square");
-
+        
       }
-       c[0].onclick = function() {
-            addclass(0,c);
-        }
-       c[1].onclick = function() {
-            addclass(1,c);
-        }
-       c[2].onclick = function() {
-            addclass(2,c);
-        }
-       c[3].onclick = function() {
-            addclass(3,c);
-        }
-       c[4].onclick = function() {
-            addclass(4,c);
-        }
-       c[5].onclick = function() {
-            addclass(5,c);
-        }
-       c[6].onclick = function() {
-            addclass(6,c);
-        }
-       c[7].onclick = function() {
-            addclass(7,c);
-        }
-       c[8].onclick = function() {
-            addclass(8,c);
-        }
-   
-
+      c[0].onmouseover = function()  {onhovering(c[0]);};
+      c[0].onmouseleave = function() {leavehovering(c[0]);};
+      c[1].onmouseover = function()  {onhovering(c[1]);};
+      c[1].onmouseleave = function() {leavehovering(c[1]);};
+      c[2].onmouseover = function()  {onhovering(c[2]);};
+      c[2].onmouseleave = function() {leavehovering(c[2]);}; 
+      c[3].onmouseover = function()  {onhovering(c[3]);};
+      c[3].onmouseleave = function() {leavehovering(c[3]);};
+      c[4].onmouseover = function()  {onhovering(c[4]);};
+      c[4].onmouseleave = function() {leavehovering(c[4]);};
+      c[5].onmouseover = function()  {onhovering(c[5]);};
+      c[5].onmouseleave = function() {leavehovering(c[5]);};
+      c[6].onmouseover = function()  {onhovering(c[6]);};
+      c[6].onmouseleave = function() {leavehovering(c[6]);};
+      c[7].onmouseover = function()  {onhovering(c[7]);};
+      c[7].onmouseleave = function() {leavehovering(c[7]);}; 
+      c[8].onmouseover = function()  {onhovering(c[8]);};
+      c[8].onmouseleave = function() {leavehovering(c[8]);};
+     
 };
 
+function leavehovering(c) {
+    c.classList.remove("hover");
+ c.onclick = null;
 
- 
-function addclass(i,c) {
-    if(c[i].className === "square X"){
-                c[i].classList.remove("X");
-                c[i].classList.add("O");
-                c[i].innerHTML='O';
+}
+ function onhovering(c){
+      console.log(c.innerHTML);
+    if(c.innerHTML == "X" || c.innerHTML == "O"){
+        c.innerHTML = c.innerHTML;}
+     else{
+    
+            if(c.className === "square hover"){
+                          c.onclick = function() {
+                    addclass(c);
+
+                               c.classList.remove("hover");
+
+                }  
+                    }else if( c.className == "square X hover"){
+                       c.onclick = function() {
+                    addclass(c);
+                            c.classList.remove("hover");
+
+                    }
+                    }else if( c.className == "square O hover"){
+                       c.onclick = function() {
+                    addclass(c);
+                            c.classList.remove("hover");
+
+                    }}
+                    else{
+                        c.classList.add("hover");
+
+                       c.onclick = function() {
+                    addclass(c);
+                      c.classList.remove("hover");
+
+                        }
+                    }  
+            
+  
+        }
+     
+ }
+function addclass(c) {
+    if(c.className === "square X"){
+                c.classList.remove("X");
+                c.classList.add("O");
+                c.innerHTML='O';
                 
-            } else if(c[i].className=== "square O"){
-                c[i].classList.remove("O");
-                c[i].classList.add("X");
-                c[i].innerHTML='X';
+            } else if(c.className=== "square O"){
+                c.classList.remove("O");
+                c.classList.add("X");
+                c.innerHTML='X';
         
             }
             else{
-                 c[i].classList.add("X");
-                c[i].innerHTML='X';
+                 c.classList.add("X");
+                c.innerHTML='X';
             }
 }
+
 
